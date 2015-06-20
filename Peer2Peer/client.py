1c5de14ba755975bf1d0919abb6b945f41e75c14
Dan Cohen <dancohen447@gmail.com>
2015-06-20 20:11:20 +0000

    Allowing client to receive long messages

--- Peer2Peer/client.py
+++ Peer2Peer/client.py
@@ -1,6 +1,24 @@
 import socket
 
-# ipAddress = socket.gethostbyname(socket.gethostname())
+def recv_end(sender):
+    total_data=[]
+    data=''
+    End = "END"
+    while True:
+            data=sender.recv(8192)
+            if End in data:
+                total_data.append(data[:data.find(End)])
+                break
+            total_data.append(data)
+            if len(total_data)>1:
+                #check if end_of_data was split
+                last_pair=total_data[-2]+total_data[-1]
+                if End in last_pair:
+                    total_data[-2]=last_pair[:last_pair.find(End)]
+                    total_data.pop()
+                    break
+    return ''.join(total_data)
+
 ipAddress = "192.168.1.205"
 port = 6699
 socketLocation = (ipAddress, port)
@@ -10,15 +28,12 @@ client.connect(socketLocation)
 
 print "Connected to port:", port
 
-message = ""
-
-for i in xrange(1,1024):
-    message += `i` + ": hello\n"
+message = "Hello"
 client.send(message)
 client.send("END")
 
 while(1):
-    response = client.recv(4096).decode()
+    response = recv_end(client)
     if(response):
         print response
         break
