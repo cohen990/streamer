52d5f38e19381de2b7734f6a5ab2867fe248f59f
Samantha Cohen <samanthacohen294@gmail.com>
2015-06-20 19:19:08 +0000

    modified to get ip address of wlan0 dynamically

--- Peer2Peer/listener.py
+++ Peer2Peer/listener.py
@@ -1,14 +1,23 @@
 import socket
+import fcntl
+import struct
 
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
 
-ipAddress = socket.gethostbyname(socket.gethostname())
+ipAddress = get_ip_address('wlan0')
 port = 6699
 socketLocation = (ipAddress, port)
 listener = socket.socket();
 listener.bind(socketLocation)
 listener.listen(5)
 
-print "Listening on port:", port
+print "Listening on port:", ipAddress, ":", port
 
 while(1):
     (clientsocket, address) = listener.accept()
